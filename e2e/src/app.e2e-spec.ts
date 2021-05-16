6a4388dcff6db6101995465ef4dcdf19122bb9ab
Mohit <mbhagat751@outlook.com>
2021-05-16 14:27:11 +0530

    corrections

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('TeamWave app is running!');
+    expect(await page.getTitleText()).toEqual('StackSearch app is running!');
   });
 
   afterEach(async () => {
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/TeamWave'),
+      dir: require('path').join(__dirname, './coverage/StackSearch'),
       subdir: '.',
       reporters: [
         { type: 'html' },
